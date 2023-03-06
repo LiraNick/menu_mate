ea78acb45505995780bfdfc6b710f7d4dec7cba0
davidsimas <david.simas81@gmail.com>
2023-03-05 21:54:46 -0300

    Implementando a view cardapio

--- estabelecimentos/views.py
+++ estabelecimentos/views.py
@@ -20,12 +20,24 @@ def contato(request):
 
 
 def cardapio(request):
+    # Recupera todos os objetos Menu do banco de dados onde o menu_id é 1.
+    menus = Menu.objects.filter(menu_id = 1)
+    # Recupera todos os objetos Menu do banco de dados.
+    #item_menus = ItemMenu.objects.all()
+    # Recupera todos os objetos ItemMenu do banco de dados que pertencem à categoria especificada
+    item_menus_lanche = ItemMenu.objects.filter(item_menu_categoria = 'Lanche')
+    item_menus_bebidas = ItemMenu.objects.filter(item_menu_categoria = 'Bebida')
+    item_menus_pastel = ItemMenu.objects.filter(item_menu_categoria = 'Pastel')
 
-    item_menus = ItemMenu.objects.all()
+    # Dicionário contento as variávies.
+    context = {
+        'menus': menus,
+        'item_menus_lanche': item_menus_lanche,
+        'item_menus_bebidas': item_menus_bebidas,
+        'item_menus_pastel': item_menus_pastel
+    }
 
-    cardapio = {'item_menus': item_menus}
-
-    return render(request, 'cardapio.html', cardapio)
+    return render(request, 'cardapio.html', context)
 
 
 def lista_estabelecimentos(request):
